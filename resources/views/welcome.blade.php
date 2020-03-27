b83b382ae4596e975a3424af42e23186e5821035
Shloka Pandya <shloka_pandya@gridle.io>
2020-03-27 20:28:01 +0530

    make:auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Dashboard
                 </div>
 
                 <div class="links">
